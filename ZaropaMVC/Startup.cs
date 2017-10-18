3af60211c1ec021b2f929ca1cfa88e57dac0bc0f
NeonElectron <gapashu@gmail.com>
2017-10-17 23:31:32 -0400

    Add project files.

---
+++ ZaropaMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZaropaMVC.Startup))]
+namespace ZaropaMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
